92a58c7dfcd43ebf15b57aa4a76f2249fdff6ddc
Maya Frame and Stephen Trewick <maya@>
2019-04-24 10:56:36 -0700

    show Program class with UI functionality

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,8 +1,65 @@
+using System;
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
-  public class Item
+  public class Program
+  {
+    public static void Main()
+    {
+    Console.WriteLine("Welcome to the To Do List.");
+    Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+    string userInput = Console.ReadLine();
 
-    //
-
+    if (userInput == "Add" || userInput == "add")
+    {
+      Console.WriteLine("Please enter the new description: ");
+      string userDescription = Console.ReadLine();
+      Item userItem = new Item(userDescription);
+      Console.WriteLine(userItem.GetDescription() + " has been added to the list!");
+      Main();
+    }
+    else if (userInput == "View" || userInput == "view")
+    {
+      foreach (Item toDoItem in Item.GetAll())
+      {
+        Console.WriteLine(toDoItem.GetDescription());
+      }
+      Main();
+    }
   }
 }
+
+    public class Item
+    {
+        private string _description;
+        private static List<Item> _instances = new List<Item> {};
+
+        public Item (string description)
+        {
+          _description = description;
+          _instances.Add(this);
+        }
+
+        public string GetDescription()
+        {
+          return _description;
+        }
+        public void SetDescription(string newDescription)
+        {
+          _description = newDescription;
+        }
+
+        public static List<Item> GetAll()
+        {
+          return _instances;
+        }
+
+        public static void ClearAll()
+        {
+          _instances.Clear();
+        }
+
+    }
+
+  }
